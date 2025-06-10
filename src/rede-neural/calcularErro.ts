9ac658e780bba0f3b4edbe01fb6dbb964647812d
Alex Yudi <yuditoma@hotmail.com>
2025-06-10 18:15:26 -0300

    refact: corrige calculo de erro para ficar com o utilizado

--- src/rede-neural/calcularErro.ts
+++ src/rede-neural/calcularErro.ts
@@ -4,29 +4,12 @@ export function calcularErro(saida: number[], alvo: number[]): number {
   // Para cada neurônio de saída
   for (let i = 0; i < saida.length; i++) {
     // Calcula o erro 
-    const erro = alvo[i] - saida[i];
-
-    // Eleva ao quadrado para eliminar valores negativos e penalizar erros maiores
-    soma += erro * erro;
-  }
-
-  // Calcula a média dos erros quadráticos
-  return soma / saida.length;
-
-  /*
-  Tipo diferente de cálculo de erro:
-  (diferença simples)
-  
-  // Para cada neurônio de saída
-  for (let i = 0; i < saida.length; i++) {
-    // Calcula o erro 
     const erro = Math.abs(alvo[i] - saida[i]);
-    
+
     // Soma a diferença absoluta
     soma += erro;
   }
-  
+
   // Retorna a soma das diferenças absolutas
   return soma / saida.length;
-  */
 }
